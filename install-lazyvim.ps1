b47c3db50291bd3a5f8a2b22a25d698fa26f6075
wangyao <1822364545@qq.com>
2025-07-07 19:57:03 +0800

    fix install-lazyvim script

--- install-lazyvim.ps1
+++ install-lazyvim.ps1
@@ -1,5 +1,5 @@
 Remove-Item -Recurse -Force -Path $env:LOCALAPPDATA\nvim
-Remove-Item -Recurse -Force -Path $env:LOCALAPPDATA\nvim.bak
+Remove-Item -Recurse -Force -Path $env:LOCALAPPDATA\nvim-data
 
 # https://www.lazyvim.org
 # git clone https://github.com/pydmy7/nvim.git $env:LOCALAPPDATA\nvim
