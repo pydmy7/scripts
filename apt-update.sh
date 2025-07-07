96796c9d4909d5059a378a680f8f5e4fe8c20edb
pydmy7 <91300046+pydmy7@users.noreply.github.com>
2025-07-07 17:33:10 +0800

    cleanup

---
+++ apt-update.sh
@@ -0,0 +1,7 @@
+sudo apt update
+sudo apt upgrade
+sudo apt full-upgrade
+
+sudo apt autoremove
+sudo apt autoclean
+sudo apt clean
---
+++ install-lazyvim.ps1
@@ -0,0 +1,5 @@
+Remove-Item -Recurse -Force -Path $env:LOCALAPPDATA\nvim
+Remove-Item -Recurse -Force -Path $env:LOCALAPPDATA\nvim.bak
+
+# https://www.lazyvim.org
+# git clone https://github.com/pydmy7/nvim.git $env:LOCALAPPDATA\nvim
