f62b99cc8cec9b854845bc721421e221c36a29b4
wangyao <wangyao@3rd-paradigm.com>
2026-05-28 17:17:35 +0800

    update

--- scripts/update-skills.ps1
+++ scripts/update-skills.ps1
@@ -1,6 +1,6 @@
 git clone --recurse-submodules https://github.com/rygo6/Vulkan-Claude.git ~/.claude/skills/vulkan
 git submodule update --init --recursive
 
-curl --create-dirs -o ~/.claude/skills/karpathy-guidelines/SKILL.md https://raw.githubusercontent.com/forrestchang/andrej-karpathy-skills/main/skills/karpathy-guidelines/SKILL.md
-
-curl --create-dirs -o ~/.claude/skills/cpp-coding-standards/SKILL.md https://raw.githubusercontent.com/affaan-m/everything-claude-code/main/skills/cpp-coding-standards/SKILL.md
+curl --create-dirs -o ~/.agent/skills/karpathy-guidelines/SKILL.md https://raw.githubusercontent.com/forrestchang/andrej-karpathy-skills/main/skills/karpathy-guidelines/SKILL.md
+curl --create-dirs -o ~/.agent/skills/cpp-coding-standards/SKILL.md https://raw.githubusercontent.com/affaan-m/ECC/main/skills/cpp-coding-standards/SKILL.md 
+curl --create-dirs -o ~/.agent/skills/python-patterns/SKILL.md https://raw.githubusercontent.com/affaan-m/ECC/main/skills/python-patterns/SKILL.md
